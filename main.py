23034e632c2640a445a3b9cda832132d24eb0c4f
Tomáš Kryčfalušij <91247418+TomasKrycfalusij@users.noreply.github.com>
2022-03-10 11:40:47 +0100

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_received_number(ReceivedNumber):
     if ReceivedNumber == 1:
+        music.play_tone(Note.B, 5000)
         basic.show_icon(IconNames.HEART)
 radio.on_received_number(on_received_number)
 
--- main.ts
+++ main.ts
@@ -9,6 +9,7 @@ input.onButtonPressed(Button.A, function on_button_pressed_a() {
 })
 radio.onReceivedNumber(function on_received_number(ReceivedNumber: number) {
     if (ReceivedNumber == 1) {
+        music.playTone(Note.B, 5000)
         basic.showIcon(IconNames.Heart)
     }
     
