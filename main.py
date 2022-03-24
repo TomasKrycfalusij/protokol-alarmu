4243b32bf97921c5e52d50dd97c4aee5a48f6910
Tomáš Kryčfalušij <91247418+TomasKrycfalusij@users.noreply.github.com>
2022-03-24 10:54:48 +0100

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,22 +1,49 @@
+learn = 0
+alarm = 0
+neighbours = []
+send = 0
 
+def on_forever():
+    remote_serial = radio.received_packet(RadioPacketProperty.SERIAL_NUMBER)
+    print(remote_serial)
+    basic.pause(1000)
+forever(on_forever)
+
+
+#MANUÁL
+# 1. Dlouhý stisk loga pro vypnutí nebo zapnutí LEARN módu
+# 2. Krátký stisk loga pro zapnutí nebo vypnutí SEND módu
+# 3. Stisknutí tlačítka A pro zapnutí alarmu
+# 4. Stisknutí tlačítka B pro vypnutí alarmu
+
+#ZAPNUTÍ/VYPNUTÍ ODESÍLACÍHO MÓDU
 def on_logo_event_longpressed():
-    pass
+    global send
+    if send == 0:
+        send = 1
+    else:
+        send = 0
+    print("Am I sending?")
+    print(send)
 input.on_logo_event(TouchButtonEvent.LONG_PRESSED, on_logo_event_longpressed)
 
+#ZAPNUTÍ LEARN MÓDU
 def on_logo_event_pressed():
-    pass
+    global learn
+    if learn == 0:
+        learn = 1
+    else:
+        learn = 0
+    print("Am I learning?")
+    print(learn)
 input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_event_pressed)
 
-
-
+#ZAPNUTÍ ALARMU
 def on_button_pressed_a():
-    radio.send_number(1)
+    pass
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
-def on_received_number(ReceivedNumber):
-    if ReceivedNumber == 1:
-        music.play_tone(Note.B, 5000)
-        basic.show_icon(IconNames.HEART)
-radio.on_received_number(on_received_number)
-
-
+#VYPNUTÍ ALARMU
+def on_button_pressed_b():
+    pass
+input.on_button_pressed(Button.B, on_button_pressed_b)
--- main.ts
+++ main.ts
@@ -1,16 +1,46 @@
+let learn = 0
+let alarm = 0
+let neighbours = []
+let send = 0
+forever(function on_forever() {
+    let remote_serial = radio.receivedPacket(RadioPacketProperty.SerialNumber)
+    console.log(remote_serial)
+    basic.pause(1000)
+})
+// MANUÁL
+//  1. Dlouhý stisk loga pro vypnutí nebo zapnutí LEARN módu
+//  2. Krátký stisk loga pro zapnutí nebo vypnutí SEND módu
+//  3. Stisknutí tlačítka A pro zapnutí alarmu
+//  4. Stisknutí tlačítka B pro vypnutí alarmu
+// ZAPNUTÍ/VYPNUTÍ ODESÍLACÍHO MÓDU
 input.onLogoEvent(TouchButtonEvent.LongPressed, function on_logo_event_longpressed() {
     
+    if (send == 0) {
+        send = 1
+    } else {
+        send = 0
+    }
+    
+    console.log("Am I sending?")
+    console.log(send)
 })
+// ZAPNUTÍ LEARN MÓDU
 input.onLogoEvent(TouchButtonEvent.Pressed, function on_logo_event_pressed() {
     
-})
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    radio.sendNumber(1)
-})
-radio.onReceivedNumber(function on_received_number(ReceivedNumber: number) {
-    if (ReceivedNumber == 1) {
-        music.playTone(Note.B, 5000)
-        basic.showIcon(IconNames.Heart)
+    if (learn == 0) {
+        learn = 1
+    } else {
+        learn = 0
     }
+    
+    console.log("Am I learning?")
+    console.log(learn)
+})
+// ZAPNUTÍ ALARMU
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
+})
+// VYPNUTÍ ALARMU
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
     
 })
